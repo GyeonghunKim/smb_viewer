69d730cf2a985861f7e6433c26644ce1378afa6e
GHKim <aass98998@snu.ac.kr>
2018-10-07 03:03:56 +0900

    gui version 2.1. function which can conversion pma movie to TIff included

--- pma_view.m
+++ pma_view.m
@@ -22,7 +22,7 @@ function varargout = pma_view(varargin)
 
 % Edit the above text to modify the response to help pma_view
 
-% Last Modified by GUIDE v2.5 30-Sep-2018 18:08:55
+% Last Modified by GUIDE v2.5 07-Oct-2018 02:52:42
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -285,3 +285,60 @@ global dirr
 cd(dirr)
 tif_name = strcat(file_name(1:end-4), sprintf('_%05d.tif', start_frame-1));
 imwrite(one_frame, hot, tif_name);
+
+
+% --- Executes on button press in pushbutton7.
+function pushbutton7_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global film_length
+global cameraType
+global xsize
+global ysize
+global file_name
+global dirr
+foldername = strcat('Tiff',file_name(1:end-4));
+cd(dirr);
+fid_pma2 = fopen(file_name,'r');
+mkdir(foldername)
+cd(foldername)
+
+if cameraType == "emCCD"
+    for i=1:film_length
+        one_frame = uint16(fread(fid_pma2,[ysize,xsize], 'uint8'));
+        name_i = sprintf('%05d',i);
+        tif_name_i = strcat(file_name(1:end-4),'_',name_i,'.tif');
+        imwrite(one_frame', tif_name_i);
+    end
+else
+    for i=1:film_length
+        one_frame = uint16(fread(fid_pma2,[ysize,xsize], 'int16'));
+        name_i = sprintf('%05d',i);
+        tif_name_i = strcat(file_name(1:end-4),'_',name_i,'.tif');
+        imwrite(one_frame', tif_name_i);
+    end 
+end
+
+
+
+function edit4_Callback(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit4 as text
+%        str2double(get(hObject,'String')) returns contents of edit4 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
